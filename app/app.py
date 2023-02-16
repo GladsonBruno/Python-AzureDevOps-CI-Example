7475e3ffefa1004ec0c6c48d353559dd8f996028
Gladson Bruno <gladson.santos-terceiro@cea.com.br>
2023-02-16 09:28:45 -0300

    Mapeando novos controllers

--- app/app.py
+++ app/app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from app.controller.UserController import user_controller_blueprint
+from app.controller.HealthController import health_controller_blueprint
 from flask_wtf.csrf import CSRFProtect
-import os
-
 
 def create_app():
     app = Flask(__name__)
+    app.register_blueprint(user_controller_blueprint)
+    app.register_blueprint(health_controller_blueprint)
 
     csrf = CSRFProtect()
     csrf.init_app(app)
